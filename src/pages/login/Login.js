62d5c94119fb44af93ffe5873fcb53335964af4e
Ken Horlador <kenhorlador@gmail.com>
2022-04-11 18:48:53 +0800

    add login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,10 +1,48 @@
+import { useState } from 'react'
 import styles from './Login.module.css'
 
+
+
 function Login() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    console.log(email, password)
+  }
+
   return (
-    <div style={ styles }>
-      Login
-    </div>
+    <form
+      className={ styles['login-form'] }
+      onSubmit={ handleSubmit }
+      >
+
+      <h2>Login</h2>
+
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          onChange={ e => setEmail(e.target.value) }
+          value={ email }
+         />
+      </label>
+
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          onChange={ e => setPassword(e.target.value) }
+          value={ password }
+        />
+      </label>
+
+      <button className='btn'>Log In</button>
+
+    </form>
   )
 }
 
