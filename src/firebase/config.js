e0cd72c57349a9e08a6af942a6d7b8936db9f156
Ken Horlador <kenhorlador@gmail.com>
2022-04-12 22:01:28 +0800

    initialize firebase and its services

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBaor4j3tWR6vQNdUlf275C0PI_T3By9jQ",
+  authDomain: "finance-tracker-aa783.firebaseapp.com",
+  projectId: "finance-tracker-aa783",
+  storageBucket: "finance-tracker-aa783.appspot.com",
+  messagingSenderId: "191669383853",
+  appId: "1:191669383853:web:0d8a9f97ce9f5f45c70c24"
+};
+
+// Initialize firebase
+firebase.initializeApp(firebaseConfig)
+
+// Initialize services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+export { projectFirestore, projectAuth }
