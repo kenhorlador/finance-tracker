6ec452d983a1aa9ba7149b6fb2b06de1300b00e7
Ken Horlador <kenhorlador@gmail.com>
2022-04-27 02:48:00 +0800

    grab user's uid

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,23 @@
-import styles from './Home.module.css'
+// Context
+import { useAuthContext } from '../../hooks/useAuthContext'
+
+// Components
 import TransactionForm from './TransactionForm'
 
+// Styles
+import styles from './Home.module.css'
+
+
 function Home() {
+  const { user } = useAuthContext()
+
   return (
     <div className={ styles['container'] }>
       <div className={ styles['content'] }>
         Transaction List
       </div>
       <div className={ styles['sidebar'] }>
-        <TransactionForm />
+        <TransactionForm uid={ user.uid } />
       </div>
     </div>
   )
