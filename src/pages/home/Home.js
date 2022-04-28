43af2ee327faf9d9f65299320e837c5d50a38f02
Ken Horlador <kenhorlador@gmail.com>
2022-04-28 23:52:16 +0800

    use 'TransactionList' component in Home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,18 +3,25 @@ import { useAuthContext } from '../../hooks/useAuthContext'
 
 // Components
 import TransactionForm from './TransactionForm'
+import TransactionList from './TransactionList'
 
 // Styles
 import styles from './Home.module.css'
 
+// Hooks
+import { useCollection } from '../../hooks/useCollection'
+
 
 function Home() {
   const { user } = useAuthContext()
+  const { documents, error } = useCollection('transactions')
 
   return (
     <div className={ styles['container'] }>
       <div className={ styles['content'] }>
-        Transaction List
+        Transactions List
+        {error && <p>{ error }</p>}
+        {documents && <TransactionList transactions={ documents } />}
       </div>
       <div className={ styles['sidebar'] }>
         <TransactionForm uid={ user.uid } />
